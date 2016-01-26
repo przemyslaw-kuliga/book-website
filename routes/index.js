c921d04578dd40a354666ee3aa8bdd38b55e4284
Przemysław Kuliga <przemyslaw.kuliga@schibsted.pl>
2016-01-26 15:43:20 +0100

    update

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,23 @@
 var express = require('express');
-var goodGuy = require('good-guy-http')({});
+var goodGuy = require('good-guy-http')({
+    maxRetries: 3
+});
+var jp = require('jsonpath');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 }).get('/book/:isbn', function(req, res, next) {
-    var isbn = req.params.isbn; //0596805527
+    var isbn = req.params.isbn;
 
     goodGuy('https://book-catalog-proxy-2.herokuapp.com/book?isbn=' + isbn).then(function (response) {
         if (response.statusCode == 200) {
-            var body = JSON.parse(response.body);
+            var book = JSON.parse(response.body).items[0];
 
             res.render('book', {
-                book: body.items[0]
+                title: jp.query(book, '$..title'), //book.volumeInfo.title,
+                cover: jp.query(book, '$..thumbnail') //book.volumeInfo.imageLinks.thumbnail
             });
         }
     });
